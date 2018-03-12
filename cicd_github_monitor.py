ffe14e7d275ac861b01f27a36b860c931ea766ee
Dorian Kersch <djkersch@gmail.com>
2018-03-11 23:33:08 -0700

    add cred example

--- cicd_github_monitor.py
+++ cicd_github_monitor.py
@@ -8,27 +8,31 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 # log into github
+g = Github(login_or_token='<insert creds>',
+           password=None,
+           base_url="https://api.github.com")
+# specify the repo you want to watch
+repo_name = 'blockchain-example'
+branch_to_monitor = 'master'
 
 while True:
-    repo_name = "blockchain-example"
     repo = g.get_user().get_repo(repo_name)
-    last_commit_sha = repo.get_branch("master").commit.sha
+    last_commit_sha = repo.get_branch(branch_to_monitor).commit.sha
     logger.info('Latest sha: ' + last_commit_sha)
     last_files_changed = repo.get_commit(last_commit_sha).files
 
     def retrieve_files_changed(last_files_changed):
-        """ # convert this into a list comprehension """
-        changed_files = []
-        for file in last_files_changed:
-            changed_files.append(file.filename)
+        """ Retrieved the files changed from a sha """
+        changed_files = [file.filename for file in last_files_changed]
         return changed_files
 
     def is_ci_needed(changed_files):
+        """ Check if we need to run CI """
         for file in changed_files:
             if file.startswith('.coverage'):
                 logger.info('no changes necessary')
             else:
-                logger.info('need to do CI stuff')
+                logger.info('Need to re-run our test suite')
                 return True
         return False
 
@@ -36,14 +40,21 @@ while True:
 
     if is_ci_needed(changed_files) == True:
         try:
+            logger.info(subprocess.check_output(
+                ['git', 'checkout', last_commit_sha, '-b' 'testing-suite']))
             logger.info(subprocess.check_output(['pytest', '--cov=.', '.']))
+            logger.info(subprocess.check_output(
+                ['git', 'checkout', branch_to_monitor]))
             logger.info(subprocess.check_output(['git', 'add', '.coverage']))
             logger.info(subprocess.check_output(
                 ['git', 'commit', '-m', '"updating tests"']))
             logger.info(subprocess.check_output(
-                ['git', 'push', 'origin', 'master']))
+                ['git', 'push', 'origin', branch_to_monitor]))
+            logger.info(subprocess.check_output(
+                ['git', 'branch', '-D', 'testing-suite']))
         except Exception as e:
-            logger.critical('Tests failed. See report above')
+            logger.critical(
+                'Tests failed. Will re-run in 60 seconds to see if they are fixed')
             exit()
     # wait 60 seconds before checking again
     time.sleep(60)
